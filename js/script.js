e11881b998426eee9e4ad678330f210ba2edb324
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-01-11 14:37:29 +0530

    Adding build folder for gh-pages

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+	$('#txtSearchText').focus();
+
+	var searchTerm = '';
+	$('#txtSearchText').keypress(function (e) {
+		if(e.which == 13) {
+			searchTerm = $(this).val();
+			console.log('searchTerm =>', searchTerm);
+			if(searchTerm){
+				APISearch(searchTerm);
+			}
+		}
+	});
+
+	function APISearch(searchTerm) {
+		$.ajax({
+			url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchTerm + "&prop=info&inprop=url&utf8=&format=json",
+			dataType: "jsonp",
+			success: function(response) {
+				console.log('response=>', response);
+				if (response.query.searchinfo.totalhits === 0) {
+					alert("No results found!");
+				} else {
+					createUI(response.query.search);
+				}
+			},
+			error: function () {
+				alert("Oops! something went wrong!");
+			}
+		});
+	}
+
+	function createUI(results) {
+		console.log('createUI=>', results);
+		$('.searchResults').empty();
+		for (var i = 0; i < results.length; i++) {
+			$('.searchResults').append('<a href="https://en.wikipedia.org/?curid='+results[i].pageid+'" target="_blank"><div class="row">'+ results[i].title +'</div></a>');
+		}
+	}
+
+});
